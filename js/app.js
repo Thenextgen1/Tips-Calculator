a508b306314fbb32657a87fc2012854851e66dfc
Elijah Ohiwerei <elijahohimairei@gmail.com>
2021-08-04 00:25:41 +0100

    refactoring all files

--- js/app.js
+++ js/app.js
@@ -5,20 +5,35 @@ const persons = document.querySelector('input[name="number"]');
 const num1 = document.querySelector('.num1')
 const num2 = document.querySelector('.num2')
 const custom = document.querySelector('.custom');
+const error = document.querySelector('.error-text');
+const mediaQuery = window.matchMedia('(min-width: 370px)');
+
+
+
 const convert = (value) => {
     return parseFloat(value.toFixed(3));
 }
+const personal = (value) => {
+    persons.addEventListener('input', () => {
+        let total = value * parseFloat(persons.value);
+        let total2 = parseFloat(total.toFixed(2));
 
+        if (persons.value === '') {
+            num2.innerHTML = '$0.00'
+        }
+        else if (persons.value) {
+            num2.innerHTML = `$${total2}`;
+        }
+    })
+}
 
 const percentCalculator = (percentages, values) => {
     return (percentages / 100) * values
 }
 
 
-
-
 buttons.forEach((elem) => {
-    elem.addEventListener('click', () => {
+    elem.addEventListener('click', (e) => {
         const Percentvals = parseInt(elem.innerHTML.slice(0, -1));
         let bills = parseInt(bill.value);
         if (typeof parseInt(persons.value) === 'number') {
@@ -30,18 +45,9 @@ buttons.forEach((elem) => {
                 num1.innerHTML = `$${totaltips}`;
             }
         }
+        e.stopPropagation();
+        personal(tipsAmt);
 
-        persons.addEventListener('input', () => {
-            let total = tipsAmt * parseFloat(persons.value);
-            let total2 = parseFloat(total.toFixed(2));
-
-            if (persons.value === '') {
-                num2.innerHTML = '$0.00'
-            }
-            else if (persons.value) {
-                num2.innerHTML = `$${total2}`;
-            }
-        })
     })
 })
 
@@ -55,6 +61,21 @@ custom.addEventListener('input', () => {
         num1.innerHTML = '$0.00'
     } else {
         num1.innerHTML = `$${totalCustom}`;
+    }
+    personal(Customamt);
+
+
+
+})
+
+persons.addEventListener('input', () => {
+    if (mediaQuery.matches && persons.value === "0") {
+        persons.classList.add('error')
+        num2.innerHTML = '$0.00'
+        error.style.display = 'inline'
+    } else {
+        persons.classList.remove('error')
+        error.style.display = 'none';
     }
 })
 
